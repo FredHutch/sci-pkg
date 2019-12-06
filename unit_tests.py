06f6a038d28789bb8cb147d976ace4f4483dcbbc
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-12-06 13:57:30 -0800

    try to add unit testing to ci/cd

---
+++ unit_tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import Mock, patch
+
+import sci
+
+# just an example - we need more
+
+
+class GetToolboxTestCase(unittest.TestCase):
+    @patch("sci.fh.requests")
+    def test_getToolbox(self, mock_requests):
+        mock = Mock()
+        mock.raise_for_status.return_value = None
+        mock.json.return_value = '{"foo": "bar"}'
+        mock_requests.get.return_value = mock
+        ret = sci.fh.getToolbox("somefile.json")
+        mock_requests.get.assert_called_once()
+        assert ret == '{"foo": "bar"}'
+
+
+if __name__ == "__main__":
+    unittest.main()
